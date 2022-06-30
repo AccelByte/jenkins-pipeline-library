c46b05824f76d538c0678f6d6e57860146b3c1dc
andriruslam <andri.wahyu@accelbyte.net>
2022-06-30 15:35:08 +0700

    test normal builder

--- vars/nodePod.groovy
+++ vars/nodePod.groovy
@@ -144,6 +144,77 @@ spec:
 """ ){ node(podLabel) { body() } }
       break
 
+    // CASE NORMALBUILDER, MOUNT DOCKER SOCK
+    case "normalbuilder":
+      podTemplate(name: podLabel,
+        label: podLabel,
+        namespace: "jenkins",
+        yaml: """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    app: jenkins-slave
+spec:
+  securityContext:
+    privileged: true
+    runAsUser: 0
+    fsGroup: 0
+  containers:
+    - name: jnlp
+      image: jenkins/jnlp-slave:alpine
+      securityContext:
+        privileged: true
+        runAsUser: 0
+        fsGroup: 0
+      tty: true
+      volumeMounts:
+        - name: dockersock
+          mountPath: "/var/run/docker.sock"
+        - name: workspace
+          mountPath: "/home/jenkins/workspace/${env.JOB_NAME}"
+        - name: tmp
+          mountPath: "/tmp"
+    - name: builder
+      image: accelbyte/golang-builder:1.12-alpine3.9
+      imagePullPolicy: IfNotPresent
+      volumeMounts:
+        - name: dockersock
+          mountPath: "/var/run/docker.sock"
+        - name: workspace
+          mountPath: "/home/jenkins/workspace/${env.JOB_NAME}"
+        - name: tmp
+          mountPath: "/tmp"
+      securityContext:
+        privileged: true
+        runAsUser: 0
+        fsGroup: 0
+      command:
+      - cat
+      tty: true
+      resources:
+        limits:
+          cpu: 2000m
+          memory: 4Gi
+        requests:
+          cpu: 64m
+          memory: 64Mi
+  volumes:
+  - name: tmp
+    hostPath:
+      path: /tmp
+  - name: dockersock
+    hostPath:
+      path: /var/run/docker.sock
+  - name: workspace
+    hostPath:
+      path: /home/jenkins/workspace/${env.JOB_NAME}
+      mode: 777
+  nodeSelector:
+    kubernetes.io/os: linux
+""" ){ node(podLabel) { body() } }
+      break
+
     // CASE DEBIAN
     case "debian":
     podTemplate(name: podLabel,
