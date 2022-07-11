ce737e152528e41fc1aa1a36d874c7a97a5b3e25
andriruslam <andri.wahyu@accelbyte.net>
2022-07-11 08:24:50 +0700

    feat: decrease resource limit

--- vars/nodePod.groovy
+++ vars/nodePod.groovy
@@ -194,8 +194,8 @@ spec:
       tty: true
       resources:
         limits:
-          cpu: 2000m
-          memory: 4Gi
+          cpu: 500m
+          memory: 1Gi
         requests:
           cpu: 64m
           memory: 64Mi
